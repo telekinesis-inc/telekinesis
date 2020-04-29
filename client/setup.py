bc5765d05fd375e64c35d13a27da8099900512a9
Elias J Neuman <elias.j.neuman@gmail.com>
2020-04-30 01:31:40 +0900

    Removed username from pip package name

--- client/setup.py
+++ client/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="camarere-elias.j.neuman", # Replace with your own username
+    name="camarere", 
     version="0.0.1",
     author="Elias J Neuman",
     author_email="elias.j.neuman@gmail.com.com",
