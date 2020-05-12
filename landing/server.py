db5e44a2931ff0de61d0839d795ea3703db9b101
Elias J Neuman <elias.j.neuman@gmail.com>
2020-05-12 19:23:44 +0900

    Opened landing server host

--- landing/server.py
+++ landing/server.py
@@ -5,7 +5,7 @@ PUBKEY = ''
 with open('PUBLIC.pub', 'r') as f:
     PUBKEY = f.read()
 
-server = Server(PUBKEY, unautheticated_message='Head over to https://cmrr.es to create an account.').serve()
+server = Server(PUBKEY, unautheticated_message='Head over to https://cmrr.es to create an account.').serve('0.0.0.0')
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(server)
