f382982d0ff72e4f5fb1b7ba1ef5e16178aa066e
Elias <elias@telekinesis.cloud>
2021-05-13 14:26:49 +0200

    Added state diffing test in python

---
+++ python/test/test_04_state_diffing.py
@@ -0,0 +1,45 @@
+from telekinesis import Broker, Telekinesis, Connection, Session, PublicUser
+import asyncio
+import pytest
+import os
+
+pytestmark = pytest.mark.asyncio
+
+
+@pytest.fixture
+def event_loop():  # This avoids 'Task was destroyed but it is pending!' message
+    yield asyncio.get_event_loop()
+
+
+async def test_state_diffing():
+    measures = {"count": 0, "size_kb": 0}
+
+    class MeasuredBroker(Broker):
+        async def handle_send(self, connection, message, source, destination):
+            measures["count"] += 1
+            measures["size_kb"] += len(message) / 2**10
+            return await super().handle_send(connection, message, source, destination)
+
+    bro = await MeasuredBroker().serve(port=8783)
+
+    class Container():
+        def __init__(self):
+            pass
+
+    container = Container()
+    c = await Connection(Session(), "ws://localhost:8783")
+    bro.entrypoint = await Telekinesis(container, c.session)._delegate("*")
+
+    pu = await PublicUser("ws://localhost:8783")._subscribe()
+
+    assert measures['size_kb'] < 2**10
+
+    container.x = os.urandom(2**20)
+
+    await pu
+    assert 2**10 < measures['size_kb'] < 1.1 * 2**10
+
+    container.y = os.urandom(2**20)
+
+    await pu
+    assert 2**11 < measures['size_kb'] < 1.1 * 2**11
