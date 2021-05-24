70274abb794e899a2b9c23acfc0e0e64e47dbfef
Elias <elias@telekinesis.cloud>
2021-05-24 12:39:46 +0200

    Version bump

--- js/src/client.ts
+++ js/src/client.ts
@@ -80,7 +80,7 @@ export class Connection {
       let sentChallenge = webcrypto.getRandomValues(new Uint8Array(32));
       let sentMetadata = new TextEncoder().encode(
         JSON.stringify({
-          version: '0.1.1'
+          version: '0.1.4'
         })
       )
 
--- python/telekinesis/broker.py
+++ python/telekinesis/broker.py
@@ -15,7 +15,7 @@ from .client import Route
 
 class Connection:
     def __init__(self, websocket):
-        self.MIN_CLIENT_VERSION = "0.1.0a20"
+        self.MIN_CLIENT_VERSION = "0.1.4"
         self.websocket = websocket
         self.logger = logging.getLogger(__name__)
         self.session = None
